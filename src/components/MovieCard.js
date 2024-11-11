3493009e5287a3c2e491f220e9c8df3fd213b252
wa1ead <waliddraa2@gmail.com>
2024-11-11 12:31:08 +0100

    styling the movie card component

---
+++ src/components/MovieCard.js
@@ -0,0 +1,29 @@
+function MovieCard(film) {
+  return (
+    <div className="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70">
+      <img
+        className="w-full h-auto rounded-t-xl"
+        src={film.poster}
+        alt={film.title}
+      />
+      <div className="p-4 md:p-5">
+        <h3 className="text-lg font-bold text-gray-800 dark:text-white">
+          {film.title}
+        </h3>
+        <p className="mt-1 text-gray-500 dark:text-neutral-400">
+          <span>{film.description}</span>
+          <br />
+          <span>{film.rating}</span>
+        </p>
+        <a
+          className="mt-2 py-2 px-3 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+          href="/"
+        >
+          Add to favoutites
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard;
