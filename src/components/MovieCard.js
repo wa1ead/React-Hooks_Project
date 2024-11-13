da02dcdc145621d7b72e7f88b0a14fa301a820cb
wa1ead <waliddraa2@gmail.com>
2024-11-13 12:41:50 +0100

    delete favorites button

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -16,12 +16,6 @@ function MovieCard({ movie }) {
         <p className="mt-1 text-yellow-500">
           Rating: <b>{movie.rating}</b>
         </p>
-        <a
-          className="mt-2 py-2 px-3 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
-          href="/"
-        >
-          Add to favoutites
-        </a>
       </div>
     </div>
   );
