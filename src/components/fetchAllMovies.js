80ea2c607cfc50beb003440b2e43a9754466d685
wa1ead <waliddraa2@gmail.com>
2024-11-20 18:41:31 +0100

    implementing fetch movies from api function

---
+++ src/components/fetchAllMovies.js
@@ -0,0 +1,24 @@
+export default async function fetchAllMovies() {
+  const url =
+    "https://api.themoviedb.org/3/discover/movie?language=en-US&sort_by=popularity.desc";
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNzI3OWNlYTY0YzQzMTM3NjllOWQ0YTI4M2Q2ZDE0YSIsIm5iZiI6MTczMjEwMTAwMC42MjY1ODYsInN1YiI6IjY3MjRjMDM1YTQ5OWNjMmVmNzA1MDFkYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gsPQH0ZHA1yJQs6XcAAxNXWXEGtMZDxN5Aez6-h5RUk",
+    },
+  };
+  try {
+    //FETCHING MOVIES DATA FROM TMDB API
+    const response = await fetch(url, options);
+    //CONVERTING PARSED DATA INTO JSON
+    const data = await response.json();
+    //EXTRACTING DATA ARRAY FROM MOVIES OBJECT
+    const moviesData = data.results;
+    console.log(moviesData);
+    return moviesData;
+  } catch (err) {
+    throw err;
+  }
+}
