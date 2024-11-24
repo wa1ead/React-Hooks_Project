94e2c50e3b7f45acbdd55a8dec39e0023c3e3878
wa1ead <waliddraa2@gmail.com>
2024-11-24 19:36:00 +0100

    adding toaster notifications

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import Home from "./components/Home";
 import Description from "./components/Description";
 import Favourites from "./components/Favourites";
 
 function App() {
   return (
-    <Routes>
-      <Route index path="/" element={<Home />} />
-      <Route path="/description/:id" element={<Description />} />
-      <Route path="/favourites" element={<Favourites />} />
-    </Routes>
+    <>
+      <Toaster />
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="/description/:id" element={<Description />} />
+        <Route path="/favourites" element={<Favourites />} />
+      </Routes>
+    </>
   );
 }
 
